de2d104f988a2aa4a94c91ffe603b057e8e214e8
George-Salt <solanikg3@gmail.com>
2022-02-23 11:45:41 +0300

    Add function get_book_attributes

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import os
 from urllib.error import HTTPError
-
+from bs4 import BeautifulSoup
 import requests
 
 
+main_url = "http://tululu.org"
 URL = "http://tululu.org/txt.php?id={id}"
+book_url = "http://tululu.org/b{id}/"
+
+
+def get_book_parameters(url, id, main_url):
+    response = requests.get(url.format(id=id))
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "lxml")
+
+    title_tag = soup.find("h1")
+    book_name = title_tag.text.split(" :: ")[0].strip()
+    author = title_tag.text.split(" :: ")[1].strip()
+
+    img_url = soup.find(class_="bookimage").find("a").find("img")["src"]
+    return f"Заголовок: {book_name}.\nАвтор: {author}. \nСсылка на картинку: {main_url}{img_url}."
 
 
 def check_for_redirect(url, id, name):
@@ -23,6 +39,8 @@ def download_book(response, name):
 
 
 try:
+    print(get_book_name_and_author(book_url, 298, main_url))
+
     os.makedirs("books", exist_ok=True)
 
     for book_num in range(10):
