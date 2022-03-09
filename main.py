82d7ea113bef993be44ac25030052541ac1878ab
George-Salt <solanikg3@gmail.com>
2022-03-09 10:45:36 +0300

    Add function download_txt

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from urllib.error import HTTPError
-from bs4 import BeautifulSoup
+
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
 main_url = "http://tululu.org"
@@ -9,41 +11,52 @@ URL = "http://tululu.org/txt.php?id={id}"
 book_url = "http://tululu.org/b{id}/"
 
 
-def get_book_parameters(url, id, main_url):
-    response = requests.get(url.format(id=id))
-    response.raise_for_status()
+def get_book_name(id, book_url):
+    response = requests.get(book_url.format(id=id))
 
     soup = BeautifulSoup(response.text, "lxml")
 
     title_tag = soup.find("h1")
     book_name = title_tag.text.split(" :: ")[0].strip()
+    return book_name
+
+
+def get_book_author(response):
+    soup = BeautifulSoup(response.text, "lxml")
+
+    title_tag = soup.find("h1")
     author = title_tag.text.split(" :: ")[1].strip()
-
-    img_url = soup.find(class_="bookimage").find("a").find("img")["src"]
-    return f"Заголовок: {book_name}.\nАвтор: {author}. \nСсылка на картинку: {main_url}{img_url}."
+    return author
 
 
-def check_for_redirect(url, id, name):
+def get_book_img_url(response, main_url):
+    soup = BeautifulSoup(response.text, "lxml")
+
+    if soup.find(class_="bookimage").find("a").find("img")["src"] == True:
+        img_url = soup.find(class_="bookimage").find("a").find("img")["src"]
+        full_img_url = f"{main_url}{img_url}"
+    return full_img_url
+
+
+def check_for_redirect(url, id, book_url):
     response = requests.get(url.format(id=id))
     if not response.history == []:
         response.raise_for_status()
     else:
-        download_book(response, name)
+        download_txt(response, get_book_name(id, book_url))
 
 
-def download_book(response, name):
-    filepath = f"books/{name}.txt"
+def download_txt(response, filename, folder = 'books/'):
+    filepath = os.path.join(folder, f"{sanitize_filename(filename)}.txt")
     with open(filepath, "wb") as file:
         file.write(response.content)
     return f"Скачано в {filepath}"
 
 
 try:
-    print(get_book_name_and_author(book_url, 298, main_url))
-
-    os.makedirs("books", exist_ok=True)
+    os.makedirs("books", exist_ok = True)
 
     for book_num in range(10):
-        check_for_redirect(URL, book_num, f"id{book_num}")
+        check_for_redirect(URL, book_num, book_url)
 except HTTPError:
     print("Такой книги не существует!")
